85316729767d73127022334fb1ebf4f07129c82c
erlgg <erlanggahernawan@gmail.com>
2025-11-04 13:23:22 +0700

    UTS SISI KLIEN

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,218 @@
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  const courses = [
-    {
-      title: "System Administration and IT Infrastructure Services",
-    },
-    {
-      title: "Operating Systems Becoming a Power User",
-    },
-    {
-      title: "The Bits and Bytes of Computer Networking",
-    },
-    {
-      title: "Technical Support Fundamentals",
-    },
-    {
-      title: "How to Succeed at: Writing Applications",
-    },
-    {
-      title: "Medicine Administration for Carers",
-    },
-  ];
+const postsData = [
+  {
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "qui est esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "eum et est occaecati",
+    body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "nesciunt quas odio",
+    body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: "dolorem eum magni eos aperiam quia",
+    body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: "magnam facilis autem",
+    body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: "dolorem dolore est ipsam",
+    body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: "nesciunt iure omnis dolorem tempora et accusantium",
+    body: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: "optio molestias id quia eum",
+    body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error",
+  },
+  {
+    userId: 2,
+    id: 11,
+    title: "et ea vero quia laudantium autem",
+    body: "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi",
+  },
+  {
+    userId: 2,
+    id: 12,
+    title: "in quibusdam tempore odit est dolorem",
+    body: "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio",
+  },
+  {
+    userId: 2,
+    id: 13,
+    title: "dolorum ut in voluptas mollitia et saepe quo animi",
+    body: "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam",
+  },
+  {
+    userId: 2,
+    id: 14,
+    title: "voluptatem eligendi optio",
+    body: "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum",
+  },
+  {
+    userId: 2,
+    id: 15,
+    title: "eveniet quod temporibus",
+    body: "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae",
+  },
+  {
+    userId: 2,
+    id: 16,
+    title:
+      "sint suscipit perspiciatis velit dolorum rerum ipsa laboriosam odio",
+    body: "suscipit nam nisi quo aperiam aut\nasperiores eos fugit maiores voluptatibus quia\nvoluptatem quis ullam qui in alias quia est\nconsequatur magni mollitia accusamus ea nisi voluptate dicta",
+  },
+  {
+    userId: 2,
+    id: 17,
+    title: "fugit voluptas sed molestias voluptatem provident",
+    body: "eos voluptas et aut odit natus earum\naspernatur fuga molestiae ullam\ndeserunt ratione qui eos\nqui nihil ratione nemo velit ut aut id quo",
+  },
+  {
+    userId: 2,
+    id: 18,
+    title: "voluptate et itaque vero tempora molestiae",
+    body: "eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam",
+  },
+  {
+    userId: 2,
+    id: 19,
+    title: "adipisci placeat illum aut reiciendis qui",
+    body: "illum quis cupiditate provident sit magnam\nea sed aut omnis\nveniam maiores ullam consequatur atque\nadipisci quo iste expedita sit quos voluptas",
+  },
+  {
+    userId: 2,
+    id: 20,
+    title: "doloribus ad provident suscipit at",
+    body: "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo",
+  },
+  {
+    userId: 3,
+    id: 21,
+    title: "asperiores ea ipsam voluptatibus modi minima quia sint",
+    body: "repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt",
+  },
+  {
+    userId: 3,
+    id: 22,
+    title: "dolor sint quo a velit explicabo quia nam",
+    body: "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse",
+  },
+  {
+    userId: 3,
+    id: 23,
+    title: "maxime id vitae nihil numquam",
+    body: "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis",
+  },
+  {
+    userId: 3,
+    id: 24,
+    title: "autem hic labore sunt dolores incidunt",
+    body: "enim et ex nulla\nomnis voluptas quia qui\nvoluptatem consequatur numquam aliquam sunt\ntotam recusandae id dignissimos aut sed asperiores deserunt",
+  },
+  {
+    userId: 3,
+    id: 25,
+    title: "rem alias distinctio quo quis",
+    body: "ullam consequatur ut\nomnis quis sit vel consequuntur\nipsa eligendi ipsum molestiae et omnis error nostrum\nmolestiae illo tempore quia et distinctio",
+  },
+];
+
+function PostCard({ title, body }) {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleButtonClick = () => {
+    setIsClicked(true);
+  };
 
   return (
-    <>
-      <div className="p-1 grid grid-cols-5 md:grid-cols-5 gap-2">
-        {courses.map((course, index) => (
-          <div key={index} 
-          className="shadow rounded-lg overflow-hidden flex flex-col
-          hover:border-b-gray-500 hover:shadow-lg hover:scale-105 transition-transform duration-300 ">
-            
-            {/* Image */}
-            <img
-              src="https://www.placehold.co/200x400"
-              alt={course.title}
-              className="w-full h-44 object-cover"
-            />
+    <div
+      className="shadow rounded-lg overflow-hidden flex flex-col
+                 transition-all duration-300 ease-in-out
+                 hover:scale-105 hover:shadow-lg hover:border hover:border-pink-300 hover:bg-pink-50"
+    >
+      <div className="p-4 flex flex-col flex-grow ">
+        <h3 className="text-sm font-semibold mb-2 min-h-[40px]">
+          {typeof title === "string" ? title : "postData"}
+        </h3>
+        <p className="text-xs text-gray-600 mb-4">
+          {body ? body.replace(/\n/g, " ") : ""}
+        </p>
 
-            {/* Content */}
-            <div className="p-4 bg-red-100 flex flex-col justify-between max-h-full">
-              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
-              <div className="bg-red-50 p-2 rounded-lg">
-                <div className="text-sm text-gray-500 mt-2">
-                  <span>👥 123 users</span>
-                  <span>⏱ 60 min</span>
-                </div>
+        <div className="flex-grow" />
+        <div className="mt-4 flex">
+          <button
+            onClick={handleButtonClick}
+            className={`w-full px-4 py-2 rounded text-sm text-white transition-colors
+              ${
+                isClicked
+                  ? "bg-[#b83016] hover:bg-red-800"
+                  : "bg-gray-500 hover:bg-gray-600"
+              }
+            `}
+          >
+            {isClicked ? "Tombol sudah diklik" : "Silakan Klik"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-                {/* Author */}
-                <div className="mt-3 mb-2 flex items-center">
-                  <img
-                    src="https://www.placehold.co/50x50"
-                    alt="Author's Avatar"
-                    className="w-12 h-12 rounded-full object-cover"
-                  />
-                  <div>
-                    <p className="text-sm font-medium">Author's Name</p>
-                    <p className="text-xs text-gray-500">Designer</p>
-                  </div>
-                </div>
-              </div>
-
-              {/* Footer */}
-              <div className="mt-4 flex items-center justify between">
-                <button className="bg-gray-500 text-white hover:bg-red-900 active:bg-red-500 px-4 py-2 rounded text-sm">
-                  silahkan klik
-                </button>
-              </div>
-            </div>
-          </div>
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-center mb-8 text-red-700">
+        Post Cards
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        {postsData.map((post) => (
+          <PostCard
+            key={post.id}
+            id={post.id}
+            userId={post.userId}
+            title={post.title}
+            body={post.body}
+          />
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
